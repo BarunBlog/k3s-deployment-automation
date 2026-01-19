e9e46508f4cee12a202d67ed16c65a4c32ead8f1
BarunBlog <bhattacharjeebarun25@gmail.com>
2026-01-19 12:16:58 +0600

    Updated the join cluster bash to install aws cli v2

--- infra/k3s-cluster/worker/scripts/join_cluster.sh
+++ infra/k3s-cluster/worker/scripts/join_cluster.sh
@@ -2,9 +2,19 @@
 
 set -e # Exit immediately if a command fails
 exec > /var/log/k3s-install.log 2>&1 # Log everything to this file for debugging
+
 # Install dependencies
 apt-get update -y
-apt-get install -y curl wget apt-transport-https ca-certificates awscli
+apt-get install -y curl wget apt-transport-https ca-certificates unzip
+
+# Install AWS CLI v2 officially
+if ! command -v aws &> /dev/null; then
+    echo "Installing AWS CLI..."
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip -q awscliv2.zip
+    sudo ./aws/install
+    rm -rf awscliv2.zip ./aws
+fi
 
 BUCKET_NAME="REPLACE_ME_BUCKET_NAME" # This name will be changed dynamically
 
